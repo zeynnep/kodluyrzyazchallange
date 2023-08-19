494b7e9961c30ea3c7fd79f20d83e5e002cd4f7a
Zeynep Atik <zeynepatik2003@gmail.com>
2023-08-19 13:14:48 +0300

    easy.py yüklenişi

---
+++ easy.py
@@ -0,0 +1,11 @@
+# Kullanıcıdan iki sayıyı al
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+# İki sayıyı topla
+toplam = sayi1 + sayi2
+
+# Sonucu ekrana yazdır
+print("İki sayının toplamı:", toplam)
+
+# Bu kod kullanıcıdan iki sayı alır, bu sayıları toplar ve sonucu ekrana yazdırır
