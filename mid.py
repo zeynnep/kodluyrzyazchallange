4191cfd34d623543c3f26bbe8c06530cd27a7ab3
Zeynep Atik <zeynepatik2003@gmail.com>
2023-08-19 13:17:14 +0300

    mid.py yüklenişi

---
+++ mid.py
@@ -0,0 +1,5 @@
+metin = input("Bir metin girin: ")  # Kullanıcıdan metni al
+
+kelime_sayisi = len(metin.split())  # Metni boşluklara göre böler ve kelime sayısını hesaplar
+
+print("Metindeki kelime sayısı:", kelime_sayisi)
